350e03ab488180035f4419aa754c1f29ad6c6c36
Cathy <Cathy347Le@gmail.com>
2019-04-23 22:27:13 -0400

    added code for index.js and app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,46 @@
-import React from "react";
-import logo from "../../logo.svg";
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import NewContact from "../NewContact/NewContact";
+import ContactList from "../ContactList/ContactList";
+import { Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contacts: props.contacts };
+
+    this.handleNewContact = this.handleNewContact.bind(this);
+  }
+
+  //remember props in setState should be in curly braces
+  //{...props} passes the router props to NewContact - history, location, and match
+
+  handleNewContact(newContact) {
+    let currentState = this.state.contacts;
+    let newContactList = currentState.push(newContact);
+    this.setState({ newContactList });
+    console.log("Test handleNewContact", this.state.contacts);
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <div className="App">
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={props => <ContactList contacts={this.state.contacts} />}
+        />
+        <Route
+          path="/new-contact"
+          render={props => (
+            <NewContact {...props} onNewContact={this.handleNewContact} />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
